cbcd004e1c67e4204ae5c3b14181bd38f1b82be3
sivakumar <sivakumarsun15@gmail.com>
2025-05-11 13:33:36 +0530

    Basename removed

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     <div className="App" style={{backgroundColor:"black"}}>
-      <BrowserRouter basename='/Portfolio'>
+      <BrowserRouter>
       <ScrollToTop/>
       <Header />
       
